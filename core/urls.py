95e8ed51ea89e5ea45769d1d9b2eb8cd3e298eb5
mjbowden <mjbowden@quinnipiac.edu>
2015-09-15 19:23:12 +0000

    Add Bootstrap

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^$', Home.as_views(), name='home'),
+   url(r'^$', Home.as_view(), name='home'),
  )
